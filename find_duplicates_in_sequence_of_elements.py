9188179cced90c2f8ac40fac1fc46df5e1ed6d75
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-02-24 22:24:03 +0000

    tried solving with clearer version of O(n) time, O(1) space solution to FindDuplicatesInSequenceOfElements problem

---
+++ find_duplicates_in_sequence_of_elements.py
@@ -0,0 +1,68 @@
+class FindDuplicatesInSequenceOfElements:
+
+
+    #Finding Duplicates in Sequence Elements of Problem
+    #Finding Duplicate Rows in Database Table is a more specific instance of that problem
+    #in practice, you may have to look at your space complexity and figure out how much RAM/disk usage that would become
+    def findDuplicates(self, nums):
+        #Approach 1:
+        #use a hashmap to keep track of num:count
+        #res = []
+        #map = {}
+        #for num in  nums
+        #if num in map
+        #   map[num] += 1
+        #else 
+        #   map[num] = 1
+        #for key in map
+        #   if map[key] > 1
+        #       res.append(map[key])
+        #return res
+        #time: O(n)
+        #space: O(n) 
+
+        #Approach 2:
+        #iterate through nums
+        #   add elements to set called found
+        #   if curr_num in found
+        #       result.append(curr_num)
+        #time: O(n)
+        #space: O(n)
+
+        #Approach 3:
+        # Given n == nums.length and 1 <= nums[i] <= n 
+        # iterate through each idx, num in nums
+        #   if abs(num) >= 0
+        #       nums[abs(num)-1] *=  -1 #marking the value at index=num - 1 as negative means num was visited
+        #   if num < 0:
+        #       result.append(idx + 1)    
+        res = []
+        for idx, num in enumerate(nums):
+            if num >= 0:
+                index = num-1 
+                nums[index] *= -1 #[4,3,-2,-7,8,2,3,1]
+            if num < 0:  
+                res.append(idx + 1)
+        return res    
+
+
+if __name__== '__main__':
+    findDuplicatesInSequenceOfElements = FindDuplicatesInSequenceOfElements()
+
+    nums1 = [4,3,2,7,8,2,3,1]
+    expectedOutput1 = [2,3]
+    actualOutput1 = findDuplicatesInSequenceOfElements.findDuplicates(nums1)
+    print(actualOutput1)
+    assert expectedOutput1 == actualOutput1, "failed test case 1 of findDuplicates"
+
+
+    nums2 = [1,1,2]
+    expectedOutput1 = [1]
+    actualOutput1 = findDuplicatesInSequenceOfElements.findDuplicates(nums2)
+    assert expectedOutput1 == actualOutput1, "failed test case 2 of findDuplicates"
+
+    nums = [1]
+    expectedOutput1 = []
+    actualOutput1 = findDuplicatesInSequenceOfElements.findDuplicates(nums)
+    assert expectedOutput1 == actualOutput1, "failed test case 3 of findDuplicates"
+
