b40d9f02b5dde231be3ffafb51884b2e11785dd0
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-02-24 16:12:04 +0000

    modified tests to try with lists with random rumber of sizes [0,200), etc.

--- just-iterate/binsearch.py
+++ just-iterate/binsearch.py
@@ -1,5 +1,7 @@
 import timeit
 import statistics
+from random import randint
+from matplotlib import pyplot
 
 def binsearch(nums, n):
     lo, hi = 0, len(nums)
@@ -23,28 +25,33 @@ def linear_search_iterative(nums, n):
 
 
 if __name__ == '__main__':
-    a = (0, 1, 3, 4)
-    b = (-5, -2, 0)
-    cases = (
-        # find in any position, even size
-        (a, 0, 0),
-        (a, 1, 1),
-        (a, 3, 2),
-        (a, 4, 3),
-        # find in any position, odd size
-        (b, -5, 0),
-        (b, -2, 1),
-        (b, 0, 2),
-        # fail to find
-        (a, 2, None),
-        (b, -3, None),
-    )
+    # a = (0, 1, 3, 4)
+    # b = (-5, -2, 0)
+    # cases = (
+    #     # find in any position, even size
+    #     (a, 0, 0),
+    #     (a, 1, 1),
+    #     (a, 3, 2),
+    #     (a, 4, 3),
+    #     # find in any position, odd size
+    #     (b, -5, 0),
+    #     (b, -2, 1),
+    #     (b, 0, 2),
+    #     # fail to find
+    #     (a, 2, None),
+    #     (b, -3, None),
+    # )
+    sizes = range(200)
     binary_search_timings = []
     linear_search_timings = []
-    for nums, n, exp in cases:
-        binary_search_timings.append(timeit.timeit(lambda: binsearch(nums, n), number=3))
-    print('ok')
-    for nums, n, exp in cases:
-        linear_search_timings.append(timeit.timeit(lambda: linear_search_iterative(nums, n), number=3))
+    for arrSize in sizes:
+        nums = [randint(0,10000) for val in range(arrSize)]
+        binary_search_timings.append(timeit.timeit(lambda: binsearch(nums, 23), number=3))
+        linear_search_timings.append(timeit.timeit(lambda: linear_search_iterative(nums, 23), number=3))
     print("binary search took a median of " + str(statistics.median(binary_search_timings) * 1000000) + " microseconds")
     print("linear search took a median of " + str(statistics.median(linear_search_timings) * 1000000) + " microseconds")
+
+    pyplot.plot(sizes, binary_search_timings, linear_search_timings)
+    pyplot.savefig('comparing_binary_search_and_iterative_search_in_time_vs_num_of_items_in_list.png')
+    #There is a dip in the time it takes to find 23 in a list of size 80 I think because it finds the 23 earlier in the list while searching with iterative search.
+    pyplot.show()
