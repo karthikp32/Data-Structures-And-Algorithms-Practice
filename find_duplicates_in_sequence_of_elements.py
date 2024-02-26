34be74e5e12901d43e1145ca8839e5a6ff01d39a
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-02-26 21:53:33 +0000

    wrote O(n) time, O(1) space solution

--- find_duplicates_in_sequence_of_elements.py
+++ find_duplicates_in_sequence_of_elements.py
@@ -38,11 +38,10 @@ class FindDuplicatesInSequenceOfElements:
         #       result.append(idx + 1)    
         res = []
         for idx, num in enumerate(nums):
-            if num >= 0:
-                index = num-1 
-                nums[index] *= -1 #[4,3,-2,-7,8,2,3,1]
-            if num < 0:  
-                res.append(idx + 1)
+            index = abs(num)-1 
+            if nums[index] < 0:  
+                res.append(abs(num))
+            nums[index] *= -1     
         return res    
 
 
