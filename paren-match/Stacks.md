3e42b4006b80ef83758ef05cd6689f287badf530
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-02-29 22:00:51 +0000

    came up with algorithm for finding missing closing parenthsis, and returning line no. and character no., Stacks notes, etc.

---
+++ paren-match/Stacks.md
@@ -0,0 +1,5 @@
+Classes of Problems that Stacks can be Useful for
+1) Traverse in reverse order from the order you originally traversed
+For ex. Going back to previous pages by hitting the browser's back button, undo button
+2) Giving priority to the most recent item
+For ex. Printing out values of nodes of a linked list A-> B-> C -> D in reverse order after traversing
---
+++ paren-match/racket_linter.py
@@ -0,0 +1,68 @@
+class RacketLinter:
+
+    #Problem: Matching Parenthesis Problem Variant
+    #Create a Racket linter that checks for matching parenthesis in Racket code 
+    #detects the location of the missing closing character 
+    #and returns the line and character number of the opening character
+    #Constraints:  parenthesis only includes {'(', ')', '[', ']'}
+    characters_and_their_line_and_character_number = []
+
+    # TODO: figure out how to read through each line of racket code,
+    #character by character and store the line number and character number in the line
+    # of each character
+    def read_in_racket_code(self):
+        return -1
+
+
+    #Matching Parenthesis Problem Variant
+    #Approach 1:
+    #Iterate through each character of racket code
+    #if character == '[' or character == '(':
+    #   push [character, line no., char no.] onto stack 
+    #elif character == ']' or character == ')':
+    #   open_paren_line_num_char_num = stack.pop()
+    #   open_paren = open_paren_line_num_char_num[0]
+    #   if character == ']' and open_paren != '[' or character == ')' and open_paren != '(':
+    #       line_num = open_paren_line_num_char_num[1]
+    #       char_num = open_paren_line_num_char_num[2]   
+    #       return line_num, char_num  
+    #else:
+    #   continue
+    #time: O(n)
+    #space: O(n)
+
+
+    def find_missing_closing_parenthesis(self):
+        return ''
+
+if __name__== '__main__':
+    racketLinter = RacketLinter()
+
+#   Ex. 1
+    # Input: s = "()"
+# Output: true
+    
+#   Ex. 2
+#     Input: s = "()[]{}"
+# Output: true
+
+
+#     Example 3:
+
+# Input: s = "(]"
+# Output: false
+    
+    #     Example 4:
+
+# Input: s = "["
+# Output: false
+    
+        #     Example 5:
+
+# Input: s = ")"
+# Output: false
+    
+            #     Example 6:
+
+# Input: s = "({)}"
+# Output: false
