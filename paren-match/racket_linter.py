190b4d519edf5152be61ee7e4549ba95f985b081
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-03-02 19:23:53 +0000

    able to return line num, char num of open parenthesis missing closing parenthesis

--- paren-match/racket_linter.py
+++ paren-match/racket_linter.py
@@ -10,8 +10,14 @@ class RacketLinter:
     # TODO: figure out how to read through each line of racket code,
     #character by character and store the line number and character number in the line
     # of each character
-    def read_in_racket_code(self):
-        return -1
+    # def read_in_racket_code(self, file_name):
+    #     line_num = 0
+    #     char_num = 0
+    #     with open(file_name) as fileobj:
+    #         for line in fileobj:  
+    #             for ch in line: 
+    #                 char_num += 1
+    #             line_num += 1        
 
 
     #Matching Parenthesis Problem Variant
@@ -32,11 +38,35 @@ class RacketLinter:
     #space: O(n)
 
 
-    def find_missing_closing_parenthesis(self):
-        return ''
+    def find_open_parenthesis_to_match_missing_closing_parenthesis(self, file_name):
+        stack = []
+        line_num = 1
+        char_num = 0
+        with open(file_name) as fileobj:
+            for line in fileobj:
+                for ch in line: 
+                    if ch == '[' or ch == '(':
+                        stack.append([ch, line_num, char_num])
+                    elif ch == ']' or ch == ')':
+                        open_paren_line_num_char_num = stack.pop()
+                        open_paren = open_paren_line_num_char_num[0]
+                        if ch == ']' and open_paren != '[' or ch == ')' and open_paren != '(':
+                            line_num = open_paren_line_num_char_num[1]
+                            char_num = open_paren_line_num_char_num[2]
+                            return line_num, char_num      
+                    char_num += 1
+                line_num += 1
+                char_num = 0  
+        return -1, -1
 
 if __name__== '__main__':
     racketLinter = RacketLinter()
+
+    # racketLinter.read_in_racket_code("stretch.rkt")
+
+    expected_line_and_char_num = 9,5
+    actual_line_and_char_num = racketLinter.find_open_parenthesis_to_match_missing_closing_parenthesis("stretch.rkt")
+    assert expected_line_and_char_num == actual_line_and_char_num, "failed test case 1 of find_open_parenthesis_to_match_missing_closing_parenthesis"
 
 #   Ex. 1
     # Input: s = "()"
