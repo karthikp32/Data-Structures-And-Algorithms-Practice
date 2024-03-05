fff09c0587a44b2e4fadacaa41c22bfb7ab5e148
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-03-05 22:01:50 +0000

    defined UndoBuffer interface and created starting point of LinkedListNode

---
+++ double_ended_queue/undo_buffer.py
@@ -0,0 +1,20 @@
+class UndoBuffer:
+
+    class LinkedListNode:
+        def __init__(self, val, next_node):
+            self.val = val
+            self.next_node = next_node
+
+
+    def add_to_front(val):
+        pass
+    def add_to_back(val):
+        pass
+    def pop_from_front(val):
+        pass    
+    def pop_from_back(val):
+        pass
+
+
+if __name__== '__main__':
+    linked_list_1 = UndoBuffer.LinkedListNode(1, None)
