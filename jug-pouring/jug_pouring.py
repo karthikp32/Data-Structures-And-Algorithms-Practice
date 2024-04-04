b3fc4504b814fae65f95beab9ac995b9eeda9af6
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-04-04 22:31:10 +0000

    implemented, tested and resolved some bugs

--- jug-pouring/jug_pouring.py
+++ jug-pouring/jug_pouring.py
@@ -66,8 +66,57 @@ class JugPouring:
         #depth += 1
         #time: O(d) where d = depth to reach five_gallon == 4
         #space: O()
-     
-    
+    class JugState:
 
+        def __init__(self, three_gallon, five_gallon):
+            self.three_gallon = three_gallon
+            self.five_gallon = five_gallon
+     
+    def get_smallest_steps_to_solve_jug_pouring_problem(self):
+        state_transitions = ['empty 3', 'empty 5', 'fill 3', 'fill 5', 'transfer from 3 to 5', 'transfer from 5 to 3']
+        start_state = self.JugState(0, 0)
+        bfsQ = []
+        bfsQ.append(start_state)
+        depth = 0
+        while bfsQ:
+            curr_state = bfsQ.pop()
+            print("three gallon has " + str(curr_state.three_gallon) + " and five gallon has " + str(curr_state.five_gallon) + " at the current state")
+            for transition in state_transitions:
+                print('current transition is ' + transition)
+                next_state = self.JugState(curr_state.three_gallon, curr_state.five_gallon)
+                if transition == 'empty 3':
+                    next_state.three_gallon = 0
+                elif transition == 'empty 5':
+                    next_state.five_gallon = 0
+                elif transition == 'fill 3':
+                    next_state.three_gallon = 3
+                elif transition == 'fill 5':
+                    next_state.five_gallon = 5
+                elif transition == 'transfer from 3 to 5':
+                    if next_state.three_gallon <= (5 - next_state.five_gallon):
+                        next_state.five_gallon += next_state.three_gallon
+                        next_state.three_gallon = 0
+                    else:        
+                        next_state.five_gallon = 5
+                        next_state.three_gallon -= (5-next_state.five_gallon)
+                elif transition == 'transfer from 5 to 3':
+                    if next_state.five_gallon >= (3 - next_state.three_gallon):
+                        next_state.five_gallon -= (3 - next_state.three_gallon)
+                        next_state.three_gallon = 3
+                    else:        
+                        next_state.five_gallon = 0
+                        next_state.three_gallon += next_state.five_gallon
+                if next_state.five_gallon == 4:
+                    return depth
+                print("three gallon of next state is " + str(next_state.three_gallon) + " and five gallon is " + str(next_state.five_gallon))
+                if next_state.three_gallon != 0 or next_state.five_gallon != 0:
+                    bfsQ.append(next_state)
+            depth += 1
+            # print(bfsQ)
+        return -1
+    
 if __name__ == '__main__':
-    pass
+    
+    jugPouring = JugPouring()
+    minimum_steps = jugPouring.get_smallest_steps_to_solve_jug_pouring_problem()
+    print(minimum_steps)
