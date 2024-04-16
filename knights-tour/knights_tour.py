5093aa62c0bbb8db8ca15dc0bc1175b68cc05265
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-04-16 22:04:43 +0000

    create KnightsTour class with notes, etc.

---
+++ knights-tour/knights_tour.py
@@ -0,0 +1,9 @@
+class KnightsTour:
+
+    #Class Chess Problem: Knight's Tour
+    #Find a way to land on every square in the chess board precisely once
+    #If you find it takes too long to compute this on a regular 8x8 chessboard, 
+    #try it with 5x5 first, then try to think of a way to improve your approach to get to an answer sooner.
+    pass
+if __name__ == '__main__':    
+    pass
